9dd29238007bc45b23aa62884badab697607b85d
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-29 23:33:45 -0500

    the new boston intro

---
+++ app.js
@@ -0,0 +1,4 @@
+/****************************************************
+*      The New Boston Introduction to JavaScript    *
+****************************************************/
+document.write("hey now brown cow");
